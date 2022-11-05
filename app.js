9fe91536a573f7181ba64b687ddee3f781a7ca79
tufail-jatoi <tufailjatoi96@gmail.com>
2022-11-05 00:25:21 -0700

    work

---
+++ app.js
@@ -0,0 +1,96 @@
+
+// var urdu = +prompt("Urdu Marks",0);
+// var english = +prompt("English Marks",0);
+// var islamiate = +prompt("islamiate Marks",0);
+
+// console.log("Urdu;",urdu);
+// console.log("English",english);
+// console.log("Islamiate",islamiate);
+
+// var total = urdu + english + islamiate;
+
+// console.log("Total",total)
+// var percent = total * 100 / 300;
+// console.log("Percentage",percent)
+
+// if (percent > 79 ){
+// console.log("Grade: A+")
+// }   else if(percent > 69){
+//     console.log("Grade: A")
+// }   else if(percent > 59){
+//     console.log("Grade: B")
+// }   else if(percent > 50){
+//     console.log("Grade: C")
+// }   else if(percent > 49){
+//     console.log("Grade: C")
+// }  else if(percent < 49){
+//     console.log("Grade: Fail")
+// }
+
+
+// var officer = false;
+// var assistant = true;
+
+// if (officer == true){
+//     console.log("document submitted");
+// }   else if (assistant == true){
+//     console.log("document submitted to assistant");
+// }   else {
+//     console.log("Wapsi")
+// }
+
+
+// var a = 10;
+// var b = 20;
+// var c = 5; 
+// if (a < b || a > 50){
+//     console.log("abc")
+// }
+
+// var a = 10;
+// var b = 20;
+// var c = 5; 
+// if (a < b && b > c){
+//     console.log("abc")
+// }
+
+// var age = 18;
+// var studentcard = true
+
+// if(age == 18){
+//     console.log("Allowed")
+// }   else {
+//     if(studentcard == true){
+//         console.log("on Student card")
+//     }   else{
+//     console.log("not allowed")
+//     }
+// }
+
+// var age = 18;
+// var studentcard = true;
+// var gender = 'male';
+
+// if(age == 18 && gender == 'male'){
+//     console.log("Allowed")
+// }   else {
+//     if(studentcard == true){
+//         console.log("on Student card")
+//     }   else{
+//     console.log("not allowed")
+//     }
+// }
+
+// var age = 18;
+// var studentcard = true;
+// var gender = 'male';
+
+// if(age == 18 || gender == 'male'){
+//     console.log("Allowed")
+// }   else {
+//     if(studentcard == true){
+//         console.log("on Student card")
+//     }   else{
+//     console.log("not allowed")
+//     }
+// }
